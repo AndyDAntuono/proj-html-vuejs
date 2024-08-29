1d6c4855598a6a3ce2f8ef4996315edf44873d62
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-29 17:58:56 +0200

    [UPD] updated AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,3 +1,28 @@
 <script></script>
-<template lang=""></template>
+<template>
+    <!--per il momento inserisco un impaginazione generica per verificare se l'output generale del progetto corrisponde all'impaginazione che sto impostando-->
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <ul>
+                        <li>Voce 1</li>
+                        <li>Voce 2</li>
+                        <li>Voce 3</li>
+                        <li>Voce 4</li>
+                        <li>Voce 5</li>
+                    </ul>
+                </div>
+                <div class="col-9">
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, saepe. Earum voluptatem libero quae repudiandae, exercitationem facilis veritatis ducimus quia quam deleniti dicta. Omnis excepturi quidem pariatur exercitationem veniam nostrum!
+                    </p>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, saepe. Earum voluptatem libero quae repudiandae, exercitationem facilis veritatis ducimus quia quam deleniti dicta. Omnis excepturi quidem pariatur exercitationem veniam nostrum!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
 <style lang=""></style>
