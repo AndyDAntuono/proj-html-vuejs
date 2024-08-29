6210285723c9a00aacfae70cc9895b09f7a7a2c4
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-29 20:17:42 +0200

    [UPD] updated index.html and AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,15 +10,18 @@
                                 <img src="../assets/img/avada-marketing-logo.png" alt="avada-marketing-logo.png">
                             </div>
                             <div class="bg-white w-80">
-                                <ul class="d-flex justify-content-between flex-row list-unstyled">
+                                <ul class="justify-content-around list-unstyled">
                                     <li class="nav-item">Home</li>
                                     <li class="nav-item">Services</li>
                                     <li class="nav-item">Why us</li>
                                     <li class="nav-item">Case-studies</li>
                                     <li class="nav-item">about</li>
                                     <li class="nav-item">Blog</li>
-                                    <li class="nav-item">7</li>
-                                    <li class="nav-item">8</li>
+                                    <li class="nav-item">
+                                        <span><i class="fa-solid fa-phone"></i></span>
+                                        <span>(555) 802-1234</span>
+                                    </li>
+                                    <li class="nav-item">Free Quotes</li>
                                 </ul>
                             </div>
                         </div>
@@ -31,5 +34,8 @@
 <style lang="scss">
     .header-navbar {
         background-color: red;
+    }
+    .fa-phone {
+        color: #f86011;
     }
 </style>
