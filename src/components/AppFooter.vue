9267f629f4b21789ea33347fb25686508d6e50b4
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-29 18:05:24 +0200

    [UPD] updated README.md and AppFooter.vue files

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,20 @@
 <script></script>
-<template lang=""></template>
+<template>
+    <!--per il momento inserisco un impaginazione generica per verificare se l'output generale del progetto corrisponde all'impaginazione che sto impostando-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="footer-navbar">
+                        <ul class="d-flex flex-row list-unstyled justify-content-end">
+                            <li class="me-2">Voce 1</li>
+                            <li class="me-2">Voce 3</li>
+                            <li class="me-2">Voce 4</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
 <style lang=""></style>
