4882241037709ccd42ce499c4a8937175b197230
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-30 16:02:25 +0200

    [UPD] updated AppFooter.vue and generals.scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,10 +4,48 @@
         <div class="container">
             <div class="row">
                 <div class="footer-navbar d-flex">
-                    <div class="fn_col-45"></div>
-                    <div class="fn_col-55"></div>
-                    <div class="fn_col-55"></div>
-                    <div class="fn_col-55"></div>
+                    <div class="fn-left">
+                        <h2>Grow Your Online Business<br> Strategically, and Improve<br> Customer Retention.</h2>
+                        <div class="mt-3">
+                            <span class="me-3"><i class="fa-brands fa-facebook-f"></i></span>
+                            <span class="me-3"><i class="fa-brands fa-twitter"></i></span>
+                            <span class="me-3"><i class="fa-brands fa-instagram"></i></span>
+                            <span class="me-3"><i class="fa-brands fa-youtube"></i></span>
+                            <span class="me-3"><i class="fa-brands fa-linkedin-in"></i></span>
+                            <span class="me-3"><i class="fa-brands fa-tiktok"></i></span>
+                        </div>
+                        <p>&copy; 2020 &#8226; Avada Consulant &#8226; Powered by WordPress</p>
+                    </div>
+                    <div class="fn-right-l">
+                        <h5 class="mb-3">Services</h5>
+                        <ul class="list-unstyled">
+                            <li class="mb-3">Marketing Plan</li>
+                            <li class="mb-3">Sales Development</li>
+                            <li class="mb-3">Digital Marketing</li>
+                            <li class="mb-3">Pricing</li>
+                            <li class="mb-3">Why Us</li>
+                            <li class="mb-3">Case Studies</li>
+                        </ul>
+                        <p>Call Us (555) 802-1234</p>
+                    </div>
+                    <div class="fn-right-c">
+                        <h5 class="mb-3">Resources</h5>
+                        <ul class="list-unstyled">
+                            <li class="mb-3">Learning Center</li>
+                            <li class="mb-3">Learning Turorials</li>
+                            <li class="mb-3">Customers</li>
+                            <li class="mb-3">Blog</li>
+                        </ul>
+                    </div>
+                    <div class="fn-right-r">
+                        <h5 class="mb-3">Company</h5>
+                        <ul class="list-unstyled">
+                            <li class="mb-3">Who We Are</li>
+                            <li class="mb-3">SContact Us</li>
+                            <li class="mb-3">Careers</li>
+                        </ul>
+                        <p>info@yourcompany.com</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,18 +55,24 @@
     .footer-navbar {
         background-color: yellow;
         width: 100%;
-        height: 500px; /*l'altezza la inserisco temporaneamente solo per impostare gli spazi*/
     };
-    .fn_col-45 {
+    .fn-left {
         width: 45%;
         background-color: pink;
-        height: 500px;
         margin-right: 10px;
     };
-    .fn_col-55 {
+    .fn-left p {
+        margin-top: 150px;
+    };
+    .fn-right-l, .fn-right-c, .fn-right-r {
         width: calc(55% / 3);
         background-color: rgb(255, 33, 70);
-        height: 500px;
         margin-right: 10px;
-    }
+    };
+    .fn-right-l p{
+        margin-top: 40px;
+    };
+    .fn-right-r p{
+        margin-top: 160px;
+    };
 </style>
