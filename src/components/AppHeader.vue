8460c9454b58442467851c1014e5e7bd6c1d0dd2
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-01 20:34:19 +0200

    [UPD] updated AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,79 +1,80 @@
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+          links: [
+            {
+              label: 'Home ',
+              url: '#',
+              current: true
+            },
+            {
+              label: 'Services',
+              url: '#',
+              current: false
+            },
+            {
+              label: 'Why us',
+              url: '#',
+              current: false
+            },
+            {
+              label: 'Case studies',
+              url: '#',
+              current: false
+            },
+            {
+              label: 'Blog',
+              url: '#',
+              current: false
+            },
+          ]
+        }
+    }
+}
+</script>
 <template>
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="navbar">
-                        <div class="logo bg-white w-20 me-2">
-                            <a href="#">
-                                <img src="../assets/img/avada-marketing-logo.png" alt="avada-marketing-logo.png">
-                            </a>
-                        </div>
-                        <div class="nav-item">
-                          <a class="nav-link" href="#">Home <i class="fa-solid fa-chevron-down"></i></a>
-                        </div>
-                        <div class="nav-item">
-                          <a class="nav-link" href="#">Services <i class="fa-solid fa-chevron-down"></i></a>
-                        </div>
-                        <div class="nav-item">
-                          <a class="nav-link" href="#">Why us</a>
-                        </div>
-                        <div class="nav-item">
-                          <a class="nav-link" href="#">Case studies</a>
-                        </div>
-                        <div class="nav-item">
-                          <a class="nav-link" href="#">Blog</a>
-                        </div>
-                        <div>
-                          <span><i class="fa-solid fa-phone me-2"></i></span>
-                          <span>(555) 802-1234</span>
-                        </div>
-                        <div class="quotes rounded nav-item">
-                          <a class="nav-link" href="#">Free Quote</a>
-                        </div>
-
-                        <!--
-                        <a class="nav-link" href="#">Home</a>
-                        <a class="nav-link" href="#">Services</a>
-                        <a class="nav-link" href="#">Why us</a>
-                        <a class="nav-link" href="#">Case studies</a>
-                        <a class="nav-link" href="#">About</a>
-                        <a class="nav-link" href="#">Blog</a>
-                        <div>
-                            <span><i class="fa-solid fa-phone me-2"></i></span>
-                            <span>(555) 802-1234</span>
-                        </div>
-                        <a class="quotes rounded" href="#">Free Quote</a>
-                        -->
-                      </div>
-                    <!--
-                    <div class="header-navbar mb-2">
-                        <div class="container-fluid d-flex align-items-center w-100">
-                            <div class="logo bg-white w-20 me-2">
-                                <img src="../assets/img/avada-marketing-logo.png" alt="avada-marketing-logo.png">
-                            </div>
-                            <div class="bg-white w-80">
-                                <ul class="justify-content-around list-unstyled">
-                                    <li class="nav-item">Home</li>
-                                    <li class="nav-item">Services</li>
-                                    <li class="nav-item">Why us</li>
-                                    <li class="nav-item">Case-studies</li>
-                                    <li class="nav-item">about</li>
-                                    <li class="nav-item">Blog</li>
-                                    <li class="nav-item">
-                                        <span><i class="fa-solid fa-phone"></i></span>
-                                        <span>(555) 802-1234</span>
-                                    </li>
-                                    <li class="nav-item">Free Quotes</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    -->
-                </div>
+  <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="navbar">
+              <div class="logo bg-white w-20 me-2">
+                <a href="#">
+                  <img src="../assets/img/avada-marketing-logo.png" alt="avada-marketing-logo.png">
+                </a>
+              </div>
+              <div v-for="link, index in links" :key="index" :class="['nav-item', link.current ? 'active' : '']">
+                <a class="nav-link" :href="link.url">{{ link.label }}</a>
+              </div>
+              <!--
+              <div class="nav-item">
+                <a class="nav-link" href="#">Home <i class="fa-solid fa-chevron-down"></i></a>
+              </div>
+              <div class="nav-item">
+                <a class="nav-link" href="#">Services <i class="fa-solid fa-chevron-down"></i></a>
+              </div>
+              <div class="nav-item">
+                <a class="nav-link" href="#">Why us</a>
+              </div>
+              <div class="nav-item">
+                <a class="nav-link" href="#">Case studies</a>
+              </div>
+              <div class="nav-item">
+                <a class="nav-link" href="#">Blog</a>
+              </div>
+              -->
+              <div>
+                <span><i class="fa-solid fa-phone me-2"></i></span>
+                <span>(555) 802-1234</span>
+              </div>
+              <div class="quotes rounded nav-item">
+                <a class="nav-link" href="#">Free Quote</a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </header>
 </template>
 <style lang="scss">
@@ -81,6 +82,11 @@
         overflow: hidden;
         background-color: #ffffff;
     }
+
+    .nav-item.active .nav-link {
+      color: #f86011;
+      border-bottom: 1px solid #f86011;
+  }
 
     .navbar .nav-link:hover {
       color: #f86011;
